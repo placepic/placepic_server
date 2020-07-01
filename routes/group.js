c16e9f91c9e29e1303effdc5a02f3e2ef69e8314
최영훈 <c_yh0327@naver.com>
2020-07-02 04:18:10 +0900

    [Feat] 지하철 : 지하철 API 모듈 추가

--- routes/group.js
+++ routes/group.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mapAPI = require('../modules/mapAPI');
+const API = require('../modules/api');
 //const groupController = require('../controllers/groups');
 
 router.get('/', async(req,res)=>{
