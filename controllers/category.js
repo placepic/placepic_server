f97b33328751cd395ef5bcb120e32e792772c1a9
JunyupHong <junyup.hong@gmail.com>
2020-07-03 04:26:34 +0900

    [Chore] controllers/categorys 파일이름 수정

---
+++ controllers/category.js
@@ -0,0 +1,17 @@
+const category = require('../models/category');
+const ut = require('../modules/util');
+const rm = require('../modules/responseMessage');
+const sc = require('../modules/statusCode');
+
+module.exports = {
+    readCategory: async (req, res) => {
+        try {
+            const allCategory = await category.getCategory();
+            return await res.status(sc.OK).send(ut.success(sc.OK, rm.READ_CATEGORY, allCategory));
+        }catch(err){
+            if(err){
+                return await res.status(sc.INTERNAL_SERVER_ERROR).send(ut.fail(sc.INTERNAL_SERVER_ERROR, rm.SERVER_ERROR));
+            }
+        }
+    }
+}
