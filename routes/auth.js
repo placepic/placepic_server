5c26568402b4f32a3003cc23514f33914860c97e
최영훈 <c_yh0327@naver.com>
2020-07-02 21:07:59 +0900

    [Refacotr] : 구조 조정

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/users');
 const authUtil  = require('../middlewares/auth').checkToken;
 
 router.post('/signup', userController.signup);
