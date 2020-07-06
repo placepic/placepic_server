74536d1fa46cc497c1a7114be13106fead07281e
JunyupHong <junyup.hong@gmail.com>
2020-07-07 03:56:16 +0900

    [Refactor] Naver Map API 연동: response property 이름 변경  #29

--- controllers/search.js
+++ controllers/search.js
@@ -12,13 +12,23 @@ const searchController = {
             let result = await api.mapFindAPI(req, res);
             const placeIdxInDB = (await placeDB.getPlacesByGroup(req.params.groupIdx, {})).map(place => place.placeIdx);
 
-            // TODO
-                // property 이름 통일 해야하는데,,,,,,,
+            console.log(result);
             result = result.map(r => {
-                r.title = r.title.replace(/<b>|<\/b>/g, '');
-                r.mobileNaverMapLink = `https://m.map.naver.com/search2/search.nhn?query=${encodeURI(r.title)}&sm=hty&style=v5#/map/1`;
-                placeIdxInDB.indexOf(r.placeId * 1) !== -1 ? r.alreadyIn = true : r.alreadyIn = false;
-                return r;
+                return {
+                    placeIdx: r.placeId,
+                    placeName: r.title.replace(/<b>|<\/b>/g, ''),
+                    placeAddress: r.address,
+                    placeRoadAddress: r.roadAddress,
+                    placeMapX: r.mapx,
+                    placeMapY: r.mapy,
+                    link: r.link,
+                    mobileNaverMapLink: `https://m.map.naver.com/search2/search.nhn?query=${encodeURI(r.title.replace(/<b>|<\/b>/g, ''))}&sm=hty&style=v5#/map/1`,
+                    alreadyIn: placeIdxInDB.indexOf(r.placeId * 1) !== -1,
+                };
+                // r.title = r.title.replace(/<b>|<\/b>/g, '');
+                // r.mobileNaverMapLink = `https://m.map.naver.com/search2/search.nhn?query=${encodeURI(r.title)}&sm=hty&style=v5#/map/1`;
+                // placeIdxInDB.indexOf(r.placeId * 1) !== -1 ? r.alreadyIn = true : r.alreadyIn = false;
+                // return r;
             });
             return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SEARCH_NAVER_MAP, result));
         } catch(e) {
