7dadc04f40830e854dcf04987b97b21b060e1327
dk-master <sunilair@naver.com>
2020-07-08 00:29:11 +0900

    [Feat] 그룹관리 1. 내 그룹조회(승인된 리스트,대기중인 리스트) 2. 관리자(내 그룹 대기중인 리스트) 3. 관리자(내 그룹 유저 승인, 거절)

--- models/group.js
+++ models/group.js
@@ -151,9 +151,42 @@ const group = {
     getGroupPostCnt : async(groupIdx) => {
         const getGroupPostCnt = `SELECT count(*) as postCount FROM PLACE_TB WHERE groupIdx = ${groupIdx}`;
 
+       
+    },
+
+    getMywaitUserList : async(groupIdx) => {
+
+        const getMywaitUserList = `SELECT * FROM (SELECT * FROM GROUP_USER_RELATION_TB WHERE groupIdx = ${groupIdx} and state = 2) AS MYGROUPWAITUSER natural join USER_TB `;
+
         try{
-            const result = await pool.queryParam(getGroupPostCnt);
-            return result[0].postCount;
+            const result = await pool.queryParam(getMywaitUserList);
+            return result;
+    
+        }catch(err) {
+            console.log('signup ERROR : ', err);
+            throw err;
+        }
+    },
+
+    editStatusApplyUser : async(userIdx,groupIdx) => {
+        const editStatusApplyUser = `UPDATE ${table} SET state = 1 WHERE userIdx = ${userIdx} and groupIdx = ${groupIdx}`;
+
+        try{
+            const result = await pool.queryParam(editStatusApplyUser);
+            return result;
+    
+        }catch(err) {
+            console.log('signup ERROR : ', err);
+            throw err;
+        }
+    },
+
+    deleteStatusApplyUser : async(userIdx,groupIdx) => {
+        const deleteStatusApplyUser = `delete FROM ${table} WHERE userIdx = ${userIdx} and groupIdx = ${groupIdx}`;
+
+        try{
+            const result = await pool.queryParam(deleteStatusApplyUser);
+            return result;
     
         }catch(err) {
             console.log('signup ERROR : ', err);
--- routes/auth.js
+++ routes/auth.js
@@ -8,4 +8,7 @@ router.post('/signup', userController.signup);
 router.post('/signin',  userController.signin);
 router.get('/groups',authUtil.checkToken, groups.getMyGroupList);
 router.post('/groups/:groupIdx/apply',authUtil.checkToken, groups.apply);
+router.get('/groups/:groupIdx/myInfo',authUtil.checkToken, groups.getMyWaitUserList);
+router.put('/groups/:groupIdx/edit', groups.editStatusApplyUser);
+router.delete('/groups/:groupIdx/delete', groups.deleteStatusApplyUser);
 module.exports = router;
