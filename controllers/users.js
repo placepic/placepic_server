eaa8558a76c0a0c064c071df68e838f759419403
JunyupHong <junyup.hong@gmail.com>
2020-07-09 06:02:26 +0900

    [Chore] 이메일 체크: 콘솔 메세지 수정

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ exports.checkEmail = async (req, res) => {
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_ID));
     }
     console.log('사용 가능한 이메일 입니다.');
-    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.ID, {email}));
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.POSSIBLE_ID, {email}));
 };
 
 exports.signup = async (req, res) => {
