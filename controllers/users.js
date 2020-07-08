c245d8ed2b84de362636a73ce0894957a01f0e2f
JunyupHong <junyup.hong@gmail.com>
2020-07-09 06:01:35 +0900

    [Fix] 이메일 체크: response message 수정

--- controllers/users.js
+++ controllers/users.js
@@ -13,8 +13,8 @@ exports.checkEmail = async (req, res) => {
         console.log('이미 존재하는 이메일 입니다.');
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_ID));
     }
-    console.log('이메일 추가 완료');
-    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.POSSIBLE_ID, {email}));
+    console.log('사용 가능한 이메일 입니다.');
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.ID, {email}));
 };
 
 exports.signup = async (req, res) => {
