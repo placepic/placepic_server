65cabde10476e2d074b20121819205c4fcbe78f7
dk-master <sunilair@naver.com>
2020-07-13 00:59:54 +0900

    [Fix] : 그룹승인 대기 유저 경로 수정

--- routes/auth.js
+++ routes/auth.js
@@ -9,9 +9,7 @@ router.post('/signup', userController.signup);
 router.post('/signin',  userController.signin);
 router.get('/groups',authUtil.checkToken, groups.getMyGroupList); // 그룹신청 페이지에서 보여줘야 할것들
 router.get('/groups/apply',authUtil.checkToken, groups.getMyApplyGroupList); //그룹 신청 가능 목록
-
 router.post('/groups/apply/:groupIdx',authUtil.checkToken, groups.apply); // 그룹신청할때
-
 router.get('/groups/admin/:groupIdx',authUtil.checkToken, groups.getMyWaitUserList); // 관리자
 router.put('/groups/admin/edit/:groupIdx', groups.editStatusApplyUser);
 router.delete('/groups/admin/delete/:groupIdx', groups.deleteStatusApplyUser);
