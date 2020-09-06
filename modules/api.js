0528f72bce24d3241bd560e27306298023102d98
JunyupHong <junyup.hong@gmail.com>
2020-09-06 17:18:16 +0900

    [Feat] NCP 메시지 발송 개발 중..

--- modules/api.js
+++ modules/api.js
@@ -1,51 +1,155 @@
-/* request query = query?=고기집 */ 
+/* request query = query?=고기집 */
 const request = require('request');
-const {client_id, client_secret} = require('../config/naverAPI.js');
-const {seoulKey} = require('../config/seoulAPI.js');
+const crypto = require('crypto');
+const moment = require('moment');
+const axios = require('axios');
+
+const { client_id, client_secret } = require('../config/naverAPI.js');
+const ncp = require('../config/naverCloudPlatform.js');
+const { seoulKey } = require('../config/seoulAPI.js');
+
 module.exports = {
-  mapFindAPI: (query) =>{
-    return new Promise((resolve, reject) =>{
-      const api_url = 'https://openapi.naver.com/v1/search/local.json?query=' + encodeURI(query) + '&display=5'; // json 결과
-      const options = {
-        url: api_url,
-        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
-      };
+    mapFindAPI: (query) => {
+        return new Promise((resolve, reject) => {
+            const api_url = 'https://openapi.naver.com/v1/search/local.json?query=' + encodeURI(query) + '&display=5'; // json 결과
+            const options = {
+                url: api_url,
+                headers: { 'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret },
+            };
 
-      request.get(options, async(error,response,body)=>{
-        const responseData = (JSON.parse(response.body).items).map(it => {
-          // link, category, description, telephone 추가 가능
-          return { placeId:it.mapx+it.mapy, title:it.title, address:it.address, roadAddress:it.roadAddress,mapx:it.mapx, mapy:it.mapy, link: it.link };
+            request.get(options, async (error, response, body) => {
+                const responseData = JSON.parse(response.body).items.map((it) => {
+                    // link, category, description, telephone 추가 가능
+                    return {
+                        placeId: it.mapx + it.mapy,
+                        title: it.title,
+                        address: it.address,
+                        roadAddress: it.roadAddress,
+                        mapx: it.mapx,
+                        mapy: it.mapy,
+                        link: it.link,
+                    };
+                });
+                // let result = [];
+                // responseData.forEach(it => {
+                //   result.push({placeId:it.mapx+it.mapy, title:it.title, address:it.address, roadAddress:it.roadAddress,mapx:it.mapx, mapy:it.mapy})
+                // });
+
+                if (error) {
+                    console.log('naver request API error : ' + error);
+                    reject(error);
+                } else {
+                    resolve(responseData);
+                }
+            });
         });
-        // let result = [];
-        // responseData.forEach(it => {
-        //   result.push({placeId:it.mapx+it.mapy, title:it.title, address:it.address, roadAddress:it.roadAddress,mapx:it.mapx, mapy:it.mapy})
-        // });
-        
-        if(error){
-          console.log('naver request API error : '+ error);
-          reject(error);
-        }
-        else {
-          resolve(responseData);
-        }
-      })
-    })
-  },
-  subWayApi: (req,res) => {
-    return new Promise((resolve, reject) => {
-      const api_uri = 'http://openapi.seoul.go.kr:8088/'+seoulKey+'/json/SearchSTNBySubwayLineInfo/0/999/%20/%20/';
+    },
+    subWayApi: (req, res) => {
+        return new Promise((resolve, reject) => {
+            const api_uri =
+                'http://openapi.seoul.go.kr:8088/' + seoulKey + '/json/SearchSTNBySubwayLineInfo/0/999/%20/%20/';
 
-      request.get(api_uri, async(error, response,body)=>{
-        let responseData = response.body;
-        console.log(responseData);
-        if(error){
-          console.log('subway request API error : ' + error);
-          reject(error);
-        }
-        else{
-          resolve(responseData)
-        }
-      })
-    })
-  }
-}
+            request.get(api_uri, async (error, response, body) => {
+                let responseData = response.body;
+                console.log(responseData);
+                if (error) {
+                    console.log('subway request API error : ' + error);
+                    reject(error);
+                } else {
+                    resolve(responseData);
+                }
+            });
+        });
+    },
+    sendMessage: (phoneNumber, certificationNumber) => {
+        return new Promise((resolve, reject) => {
+            const domain = 'https://sens.apigw.ntruss.com';
+            const url = `/sms/v2/services/${ncp.serviceId}/messages`;
+            const api_url = domain + url;
+            const timestamp = moment().valueOf().toString();
+            const signature = crypto
+                .createHmac('sha256', ncp.secretKey)
+                .update('POST')
+                .update(' ')
+                .update(url)
+                .update('\n')
+                .update(timestamp)
+                .update('\n')
+                .update(ncp.accessKey)
+                .digest('base64');
+            console.log('SIGNATURE:', signature);
+
+            const options = {
+                uri: api_url,
+                headers: {
+                    'content-type': 'application/json; charset=utf-8',
+                    'x-ncp-apigw-timestamp': timestamp,
+                    'x-ncp-iam-access-key': ncp.accessKey,
+                    'x-ncp-apigw-signature-v2': signature,
+                },
+                form: {
+                    type: 'SMS',
+                    contentType: 'COMM',
+                    countryCode: '82',
+                    from: ncp.callingNumber,
+                    content: `placepic 인증번호 ${certificationNumber}`,
+                    messages: {
+                        to: phoneNumber,
+                    },
+                },
+                json: true,
+            };
+
+            // console.log('post start', options);
+            // let form = new FormData();
+            // form.append('type', 'SMS');
+            // form.append('contentType', 'COMM');
+            // form.append('countryCode', '82');
+            // form.append('from', ncp.callingNumber);
+            // form.append('content', `placepic 인증번호 ${certificationNumber}`);
+            // form.append('messages', {
+            //     to: phoneNumber,
+            // });
+
+            let form = {
+                type: 'SMS',
+                contentType: 'COMM',
+                countryCode: '82',
+                from: ncp.callingNumber,
+                content: `placepic 인증번호 ${certificationNumber}`,
+                messages: {
+                    to: phoneNumber,
+                },
+            };
+            console.log(JSON.stringify(form));
+            // axios
+            //     .post(`https://sens.apigw.ntruss.com/sms/v2/services/${ncp.serviceId}/messages`, {
+            //         headers: {
+            //             'content-type': 'application/json; charset=utf-8',
+            //             'x-ncp-apigw-timestamp': timestamp,
+            //             'x-ncp-iam-access-key': ncp.accessKey,
+            //             'x-ncp-apigw-signature-v2': signature,
+            //         },
+            //         data: form,
+            //     })
+            //     .then((result) => {
+            //         // console.log(result);
+            //         console.log('succe');
+            //         resolve('success');
+            //     })
+            //     .catch((e) => {
+            //         reject(e);
+            //     });
+
+            request.post(options, (error, response, body) => {
+                console.log('post end');
+                if (error) {
+                    console.log('NCP message API error : ' + error);
+                    reject(error);
+                }
+                console.log(response.body, body);
+                resolve(/** 여기 리턴값이 */);
+            });
+        });
+    },
+};
